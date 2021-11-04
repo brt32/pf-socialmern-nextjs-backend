35a184754cac687053a4e438690783f8133335be
Bartek <bartlomiej.proczkowski@gmail.com>
2021-11-04 18:33:24 +0100

    CORS Client Origin updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: `"${process.env.CLIENT_URL}"`,
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-type"],
   },
